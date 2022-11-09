02875d31bafb910afb3b7137df2518132383d1f1
unknown <rabogoshvili_art@inbox.rugit config --global user.email rabogoshvili_art@inbox.rugit config --global user.name Artem-1983git commit -m 'homework-module-10-help!>
2022-11-09 13:48:03 +0300

    homework-module-4

--- js/index.js
+++ js/index.js
@@ -1,11 +1,1 @@
-window.addEventListener ('DOMContentLoaded',function(){
-  document.querySelector('#burger').addEventListener('click',function()
-  {document.querySelector('#menu-mobile').classList.add ('nav__menu-mobile-active');
-})
-
-document.querySelector('#menu-close').addEventListener('click',function()
-  {document.querySelector('#menu-mobile').classList.remove ('nav__menu-mobile-active');
-})
-})
-
-
+window.addEventListener("DOMContentLoaded",function(){lazyload(),document.querySelector("#burger").addEventListener("click",function(){document.querySelector("#menu-mobile").classList.add("nav__menu-mobile-active")}),document.querySelector("#menu-close").addEventListener("click",function(){document.querySelector("#menu-mobile").classList.remove("nav__menu-mobile-active")})});  
--- js/swiper.js
+++ js/swiper.js
@@ -10,5 +10,5 @@ const swiper = new Swiper('.hero-swiper', {
     a11y: {
       enabled: true,
       paginationBulletMessage: 'переход на слайд {{index}}'
-    }
+    },
 });
